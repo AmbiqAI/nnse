3dd9fc9bef36791582fdb24a0c646a8cd90fd8b5
poyupaulchen <poyupaulchen@gmail.com>
2023-11-14 11:37:57 -0800

    fixed

--- python/convert2tflite.py
+++ python/convert2tflite.py
@@ -2,25 +2,27 @@
 Convert the tensorflow NN weight table to C code
 """
 import argparse
-from nnsp_pack.nn_module import NeuralNetClass
-from nnsp_pack.load_nn_arch import load_nn_arch, setup_nn_folder
 import numpy as np
 import tensorflow as tf
 from pathlib import Path
 from typing import Callable
+from nnsp_pack.nn_module import NeuralNetClass
+from nnsp_pack.load_nn_arch import load_nn_arch, setup_nn_folder
 
 DIM_INPUT = 72  # size of a single sample, e.g. an embedding of size 64
-
-
 def model() -> tf.keras.Model:
     """Example of a simple single-directional LSTM. Embedding layers are not
     supported and will have to be added as pre-processing steps manually. See the
     official TFLite documentation for more information and constraints:
     https://www.tensorflow.org/lite/models/convert/rnn. See in-line comments below
     for recommendations about tf.keras.layers.LSTM()."""
+
+    # Define the inputs of net
     in_layer = tf.keras.layers.Input((6, DIM_INPUT, 1), batch_size=1)
     h_state = tf.keras.layers.Input(72, batch_size=1)
     c_state = tf.keras.layers.Input(72, batch_size=1)
+
+    # Define the layers of net
     cnn_layer = tf.keras.layers.Conv2D(
                 72,
                 (6, 72),
@@ -38,6 +40,7 @@ def model() -> tf.keras.Model:
     dense_layer1 = tf.keras.layers.Dense(72, activation='tanh')
     dense_layer2 = tf.keras.layers.Dense(257, activation='sigmoid')
 
+    # NN inference
     x = in_layer
     x = cnn_layer(x)
     x, h_state1, c_state1 = lstm_layer(x[:,:,0,:], initial_state=(h_state, c_state))
@@ -115,11 +118,13 @@ def main(args):
     """
 
     net = model() # generate the model for tflite conversion
-    in_data = np.random.randn(1 , 6, DIM_INPUT, 1)
+    in_data = np.random.randn(1 , 6, DIM_INPUT, 1) # (batch, timesteps, dim, ch)
     h_state = np.random.randn(1,72)
     c_state = np.random.randn(1,72)
-    out_data = net([in_data,h_state, c_state])
-
+    out_data, h_state_new, c_state_new = net([in_data,h_state, c_state]) # run the test once
+    print(out_data.shape)
+    print(h_state_new.shape)
+    print(c_state_new.shape)
     def dataset_example(num_samples: int = 100):
         """Placeholder for a representative data-set. For best quantization
         performance, replace this with a few examples from your own data-set, the
@@ -129,8 +134,8 @@ def main(args):
         for _ in range(num_samples):
             yield [tf.random.uniform(
                         shape_data,
-                        minval= -(2.0**15 / 2**8),
-                        maxval=((2.0**15-1) / 2**8)),
+                        minval= -2.0**15 / 2**8,
+                        maxval=(2.0**15-1) / 2**8),
                     tf.random.uniform(
                         shape_state,
                         minval=-1.0,
@@ -140,6 +145,8 @@ def main(args):
                         maxval=(2**31-1)/2**15)]
 
     net_trained = get_trained_model(args) # load already trained model
+
+    # put weight table from already trained model to new defined model
     for v in zip(net_trained.trainable_variables, net.trainable_variables):
         v1, v2 = v
         v2.assign(v1)
@@ -167,7 +174,7 @@ if __name__ == "__main__":
 
     argparser.add_argument(
         '--nbit',
-        default= 16,
+        default= 8,
         help='nbit for weight')
 
     argparser.add_argument(
