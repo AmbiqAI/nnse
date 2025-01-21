8769f4a85c4e06bb5f21cec2d422ace9c0944b5f
poyupaulchen <poyupaulchen@gmail.com>
2025-01-20 17:01:58 -0800

    update

--- python/train_se.py
+++ python/train_se.py
@@ -587,7 +587,7 @@ if __name__ == "__main__":
     argparser.add_argument(
         '-a',
         '--config_file',
-        default='nn_arch/config_unet_relu.yaml',
+        default='nn_arch/config_unet_relu_large.yaml',
         help='nn architecture')
 
     argparser.add_argument(
